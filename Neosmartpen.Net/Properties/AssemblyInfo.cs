ab82578618d43a20128069ed4f43ea0a81e95087
chyccs <>
2024-11-07 17:32:21 +0900

    fff

--- Neosmartpen.Net/Properties/AssemblyInfo.cs
+++ Neosmartpen.Net/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Neosmartpen.Net")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Amazon.com")]
+[assembly: AssemblyCompany("NeoLAB Convergence")]
 [assembly: AssemblyProduct("Neosmartpen.Net")]
-[assembly: AssemblyCopyright("Copyright © Amazon.com 2024")]
+[assembly: AssemblyCopyright("Copyright © NeoLAB Convergence 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      빌드 번호
 //      수정 버전
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyFileVersion("2.5.0")]
